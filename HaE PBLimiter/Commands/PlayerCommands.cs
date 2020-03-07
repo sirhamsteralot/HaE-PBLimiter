422a7490c25554ebfa0558154e367bf43362096c
sirhamsteralot <sirhamsteralot@gmail.com>
2020-03-07 23:30:19 +0100

    Command module

---
+++ HaE PBLimiter/Commands/PlayerCommands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+
+namespace HaE_PBLimiter.Commands
+{
+    [Category("pblimiter")]
+    public class PlayerCommands : CommandModule
+    {
+        [Command("status", "Lists pbs and their average runtimes owned by the player")]
+        [Permission(MyPromoteLevel.None)]
+        public void PBStatus()
+        {
+            var player = Context?.Player;
+
+            if (player == null)
+                return;
+
+            var sb = new StringBuilder();
+
+            foreach (var pb in PBData.pbPair.Values)
+            {
+                if (pb.PB.OwnerId == player.IdentityId)
+                    sb.AppendLine($"PB: \"{pb.PBID}\" Ms: {pb.AverageMS:F3}");
+            }
+
+            Context.Respond(sb.ToString());
+        }
+    }
+}
