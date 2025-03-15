6f8efebbfd7506734e7d2e122516f445fe053d0d
empireu <rodney.mckay.meredith@gmail.com>
2025-03-15 20:24:17 +0200

    Adds handling for GC pauses for further testing

--- HaE PBLimiter/PBProfilerPatch.cs
+++ HaE PBLimiter/PBProfilerPatch.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Diagnostics;
 using NLog;
 using Sandbox.Game.Entities.Blocks;
@@ -6,9 +7,49 @@ using Torch.Managers.PatchManager;
 using Torch.Utils;
 using Torch.Utils.Reflected;
 
-
 namespace HaE_PBLimiter
 {
+    /// <summary>
+    ///     Used to track GC generations during PB execution.
+    ///     If we find that the number of collections for a generation has increased somewhere in between the start of the PB's execution and the end of the PB's execution,
+    ///     we can look into discarding the measured time value because it's bogus (it includes the time taken by GC).
+    /// </summary>
+    internal static class GcStateManager
+    {
+        private static readonly int[] Counters;
+
+        static GcStateManager()
+        {
+            Counters = new int[GC.MaxGeneration + 1];
+        }
+        
+        public static void EnterCriticalSection()
+        {
+            var counts = Counters;
+            
+            for (var i = 0; i < counts.Length; i++)
+            {
+                counts[i] = GC.CollectionCount(i);
+            }
+        }
+
+        public static void ExitCriticalSectionAndCheckGc(out int maxGcCount)
+        {
+            maxGcCount = 0;
+            
+            var counts = Counters;
+            for (var i = 0; i < counts.Length; i++)
+            {
+                var diff = GC.CollectionCount(i) - counts[i];
+
+                if (diff > maxGcCount)
+                {
+                    maxGcCount = diff;
+                }
+            }
+        }
+    }
+    
     [ReflectedLazy]
     internal static class PBProfilerPatch
     {
@@ -19,8 +60,7 @@ namespace HaE_PBLimiter
 
         [ReflectedMethodInfo(typeof(MyProgrammableBlock), "Compile")]
         private static readonly MethodInfo _programableRecompile;
-
-
+        
         public static void Patch(PatchContext ctx)
         {
             ReflectedManager.Process(typeof(PBProfilerPatch));
@@ -36,6 +76,8 @@ namespace HaE_PBLimiter
         // ReSharper disable once SuggestBaseTypeForParameter
         private static void PrefixProfilePb(MyProgrammableBlock __instance, ref long __localTimingStart)
         {
+            GcStateManager.EnterCriticalSection();
+            
             if (ProfilerConfig.takeIngameMeasurement)
                 return;
 
@@ -52,8 +94,16 @@ namespace HaE_PBLimiter
             if (!ProfilerConfig.takeIngameMeasurement)
                 dtInSeconds = (Stopwatch.GetTimestamp() - __localTimingStart) / (double)Stopwatch.Frequency;
             else
-                dtInSeconds = (double)(lastruntimeMS.GetValue(runtimeField.GetValue(__instance)))/1000.0;
+                dtInSeconds = (double)lastruntimeMS.GetValue(runtimeField.GetValue(__instance)) / 1000.0;
 
+            GcStateManager.ExitCriticalSectionAndCheckGc(out var maxGcCount);
+
+            if (maxGcCount == 1)
+            {
+                Log.Warn($"Dropped \"{__instance.CustomName}\" for GC. Time would have been {dtInSeconds * 1e6:F}us");
+                dtInSeconds = 0.0;
+            }
+            
             PBData.AddOrUpdatePair(__instance, dtInSeconds);
         }
 
